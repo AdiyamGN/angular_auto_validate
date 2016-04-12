de0ab7f930853bead9f598369f56bc506849ef5d
AdiyamGN <adiyamgn@outlook.com>
2016-04-12 16:00:24 -0500

    added customize error messages on the run block in the form-validation js to work with the angular auto validate module. also edited the index.html page to include those error messages.

--- js/form-validation-controller.js
+++ js/form-validation-controller.js
@@ -5,6 +5,18 @@
 	angular
 		.module('angularAutoValidate')
 
+
+		/* The run function hocks in before the controller is instantiated and adds the functionality to customize 
+		our own error mesage */
+
+		.run(function(defaultErrorMessageResolver) {
+			defaultErrorMessageResolver.getErrorMessages().then(function(errorMessages) {
+				errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+				errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+				errorMessages['badUsername'] = 'Username can only contain numbers and letters and _';
+			});
+		})
+
 		.controller('AutoValidateController', AutoValidateController);
 
 
