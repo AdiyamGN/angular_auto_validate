de6e35bcac33144d66c658d99a50b585c7270074
AdiyamGN <adiyamgn@outlook.com>
2016-04-12 12:58:00 -0500

    added main module and AutoValidateController controller js files and modified index.html.

---
+++ js/form-validation-controller.js
@@ -0,0 +1,15 @@
+(function() {
+
+	'use strict';
+
+	angular
+		.module('angularAutoValidate')
+
+		.controller('AutoValidateController', AutoValidateController);
+
+
+		function AutoValidateController() {
+
+		}
+
+})();
